11a11961408a1e5c0fb4dc61a61134f222917a38
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-02-15 10:02:46 -0700

    finished

--- seed.js
+++ seed.js
@@ -1,32 +1,32 @@
-
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
-
+var express = require('express');
 var db = require('./models');
+var app = express();
 
 var new_videogames = [
 {
-	title:"Diablo 3" ,
-	console:"PC" ,
-	year: 2012,
-	multiplayer: true,
-},
-{
-	title:"Destiny" ,
-	console:"PS4" ,
-	year: 2014,
-	multiplayer: true,
-},
-{
 	title:"Denver Defenders" ,
-	console:"PC" ,
+	console:"Web Browser" ,
 	year: 2017,
 	multiplayer: false,
 },
 {
-	title:"Tetris" ,
-	console:"Arcade" ,
-	year: 1984,
+	title:"The Legend of Zelda: A Link to the Past" ,
+	console:"NES" ,
+	year: 1995,
+	multiplayer: false,
+},
+{
+	title:"Diablo 3" ,
+	console:"PS4, PC, Mac" ,
+	year: 2015,
+	multiplayer: true,
+},
+{
+	title:"Attack From Mars" ,
+	console:"Pinball/Arcade" ,
+	year: 1995,
 	multiplayer: true,
 }
 ];
--- server.js
+++ server.js
@@ -16,11 +16,11 @@ var db = require('./models');
 
 app.get('/api/profile', function(req, res){
   res.json ({
-    name: "Matt Steffey",
-    github_link: "https://github.com/mattsteffey",
+    name: "RJ",
+    github_link: "https://github.com/rjmcbain",
     github_image: "https://avatars2.githubusercontent.com/u/33466125?s=460&v=4",
     current_city: "Denver",
-    pets:[{name: "Bronson", type: "Dog", breed: "Boston Terrier"}, {name: "Bane", type: "Dog", breed: "Boston Terrier"}]
+    pets:[{name: "Ace", type: "Dog", breed: "Boxer"}]
   });
 });
 /**********
@@ -34,7 +34,54 @@ app.get('/api/profile', function(req, res){
   });
  });
 
+ app.get('/api/videogames/:id', function(req, res){
+  db.Videogames.findById(req.params.id, function(err, videogames){
+    if(err){res.send("error finding videogames", err);}
+    res.json(videogames);
+  });
+ });
 
+ app.post('/api/videogames', function(req, res){
+  db.Videogames.create(
+    { title: req.body.title,
+      console: req.body.console,
+      multiplayer: req.body.multiplayer,
+      year: req.body.year
+    }, function(err, videogames){
+    if(err){res.send("error creating videogames", err);}
+    res.json(videogames);
+  });
+ });
+
+app.put('/api/videogames/:id', function(req, res){
+  db.Videogames.findById(req.params.id, function(err, updateVideogames){
+    if(err)
+      res.json("error.",err);
+    if(updateVideogames)
+      console.log(updateVideogames);
+      updateVideogames.title = req.body.title;
+      updateVideogames.console = req.body.console;
+      updateVideogames.mutltiplayer = req.body.mutltiplayer;
+      updateVideogames.year = req.body.year;
+
+      updateVideogames.save(function (err, updateVideogames) {
+          if (err) return handleError(err);
+          res.json(updateVideogames);
+        });
+    });
+}); 
+
+app.delete('/api/videogames/:id', function(req, res){
+  db.Videogames.remove({_id: req.params.id},function(err, deleteVideogames){
+    if(err){res.send("error.",err);}
+    res.json(deleteVideogames);
+    if(deleteVideogames);
+      console.log(deleteVideogames);
+      deleteVideogames.delete(function(err, deleteVideogames){
+        res.json(deleteVideogames);
+      });
+  });
+});
 
 // Serve static files from the `/public` directory:
 // i.e. `/images`, `/scripts`, `/styles`
@@ -56,18 +103,20 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    message: "Thank you for visiting my API",
-    documentation_url: "https://github.com/mattsteffey/express-personal-api/blob/master/README.md", // CHANGE ME
-    base_url: "https://whispering-ravine-69221.herokuapp.com/", // CHANGE ME
+    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
+    message: "Welcome to my personal api! Here's what you need to know!",
+    documentation_url: "https://github.com/rjmcbain/express-personal-api/blob/master/README.md", // CHANGE ME
+    base_url: "https://dry-tundra-96948.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "About Me"}, // CHANGE ME
-      {method: "POST", path: "/api/videogames", description: "Create a new video game"}, // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Dis About Me"}, // CHANGE ME
+      {method: "POST", path: "/api/videogames", description: "E.g. Create a new video game"}, // CHANGE ME
       {method: "DELETE", path: "/api/videogames/:id", description: "Delete a video game"}
     ]
-  });
+  })
 });
 
+//
 
 
 
@@ -79,3 +128,4 @@ app.get('/api', function api_index(req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
 });
+
